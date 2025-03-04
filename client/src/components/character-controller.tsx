6a29543186641660935e5b981b96cf92c234b2df
NunesClement <nunes.clement19@gmail.com>
2025-03-04 22:58:59 +0100

    fix

--- client/src/components/character-controller.tsx
+++ client/src/components/character-controller.tsx
@@ -40,7 +40,7 @@ export function CharacterController() {
       WALK_SPEED: { value: 1.5, min: 0.1, max: 4, step: 0.1 },
       RUN_SPEED: { value: 2.1, min: 0.2, max: 12, step: 0.1 },
       ROTATION_SPEED: {
-        value: degToRad(0.1),
+        value: 0.02,
         min: degToRad(0.1),
         max: degToRad(5),
         step: degToRad(0.1),
