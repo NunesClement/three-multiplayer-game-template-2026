6ba9ee860dbe408b02a6c59699dec4e39bb0ef9f
NunesClement <nunes.clement19@gmail.com>
2025-03-07 20:28:21 +0100

    fix

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { AnimationCharacterType, Character, Chat } from "../common-interfaces";
 
 const io = new Server({
   cors: {
-    origin: "https://a-game-test.vercel.app",
+    origin: "*",
     // import.meta.env.NODE_ENV === "development"
     //   ? "http://localhost:5173"
     //   : "http://localhost:5173",
