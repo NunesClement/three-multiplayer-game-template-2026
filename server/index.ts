800f4f3d38771f755958731b7dcd6362cfc4f529
NunesClement <nunes.clement19@gmail.com>
2025-03-07 15:55:41 +0100

    fix

--- server/index.ts
+++ server/index.ts
@@ -26,8 +26,13 @@ httpServer.listen(3003, "0.0.0.0", () => {
   console.log("Socket.io server running on port 3003");
 });
 
-console.log("TEST");
-
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+  socket.on("message", (msg) => {
+    console.log("Received:", msg);
+    io.emit("message", msg);
+  });
+});
 // io.listen(3003);
 
 const characters: Character[] = [];
